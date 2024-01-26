cad77eace9a393208b6dc3412551b9e5cdc3cadf
Arjun0034 <141438736+Arjun0034@users.noreply.github.com>
2024-01-26 21:48:57 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,23 @@
+function openProjectPage(Project1) {
+    var projectPageURL = Project1 + '.html';
+    window.open(projectPageURL, '_blank');
+}
+
+function openProjectPage(Project2) {
+    var projectPageURL = Project2 + '.html';
+    window.open(projectPageURL, '_blank');
+}
+
+function openProjectPage(Project3) {
+    var projectPageURL = Project3 + '.html';
+    window.open(projectPageURL, '_blank');
+}
+
+function openProjectPage(Project4) {
+    var projectPageURL = Project4 + '.html';
+    window.open(projectPageURL, '_blank');
+}
+
+
+
+
